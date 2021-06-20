bed7872149a2c5c160e63e35f6a4a768f16942aa
Sergey <stepanyuk.sergius@gmail.com>
2021-06-21 01:02:21 +0300

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface JsTech {
+  value: string;
+  viewValue: string;
+  version: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-web-app';
+  selected = -1;
+  jstechs: JsTech[] = [
+    {value: '0', viewValue: 'Angular', version:[{v:'1.1.1'}, {v:'1.2.1'}, {v:'1.3.3'}]},
+    {value: '1', viewValue: 'React', version:[{v:'2.1.2'}, {v:'3.2.4'}, {v:'4.3.1'}]},
+    {value: '2', viewValue: 'Vue', version:[{v:'3.3.1'}, {v:'5.2.1'}, {v:'5.1.3'}]}
+  ];
 }
